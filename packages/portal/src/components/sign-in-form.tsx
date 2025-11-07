3c1687f80bdbd8200fe8594e4368d869ee485cfa
Aleksandr Zelentsov <aazelentsov@gid.ru>
2025-11-07 23:53:43 +0300

    Добавление Tolgee в портал (отладка на базе формы Логина)

--- packages/portal/src/components/sign-in-form.tsx
+++ packages/portal/src/components/sign-in-form.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {useTranslate} from "@tolgee/react";
 
-export function LoginForm({ className, ...props}: React.ComponentProps<"div">) {
+export function SignInForm({className, ...props}: React.ComponentProps<"div">) {
   const { t, isLoading } = useTranslate();
   
   console.log(isLoading)
