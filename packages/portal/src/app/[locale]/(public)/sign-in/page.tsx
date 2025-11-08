c311a99543355ad2d3155facaa5268307527ceaa
Aleksandr Zelentsov <aazelentsov@gid.ru>
2025-11-08 13:30:30 +0300

    Подключение авторизации через Keycloak и Next-Auth 5 + добавление лайаута на закрытую часть портала

--- packages/portal/src/app/[locale]/(public)/sign-in/page.tsx
+++ packages/portal/src/app/[locale]/(public)/sign-in/page.tsx
@@ -1,27 +1,18 @@
-import {SignInForm} from "@/components/sign-in-form";
 import {signIn} from "@/auth";
+import SignInKeycloak from "@/components/auth/sign-in/sign-in-keyckloak";
 
+async function handleLogin() {
+  'use server';
+  await signIn('keycloak');
+}
 
 export default function SignInPage() {
-  async function handleLogin(_: unknown, formData: FormData) {
-    'use server';
-    
-    const username = formData.get('username') as string;
-    const password = formData.get('password') as string;
-    
-    // напрямую вызвать Keycloak логин через signIn
-    await signIn('keycloak', {
-      // если в Keycloak настроен password flow
-      username,
-      password,
-      redirectTo: '/dashboard',
-    });
-  }
   
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <SignInForm />
+        {/*<SignInForm />*/}
+        <SignInKeycloak onSubmit={handleLogin}/>
       </div>
     </div>
   )
--- packages/portal/src/auth.ts
+++ packages/portal/src/auth.ts
@@ -1,14 +1,11 @@
 import NextAuth from "next-auth"
 import Keycloak from "next-auth/providers/keycloak"
 
-const issuer = process.env.AUTH_KEYCLOAK_ISSUER!;
-const clientId = process.env.AUTH_KEYCLOAK_ID!;
-// const clientSecret = process.env.AUTH_SECRET;
-
-
 export const { handlers, signIn, signOut, auth } = NextAuth(req => {
   if (req) {
     console.log(req)
   }
-  return { providers: [Keycloak] }
+  return {
+    providers: [Keycloak]
+  }
 })
