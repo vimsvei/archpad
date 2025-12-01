4a91ae39f7698bd02e93e97972955ffbcd70c060
Aleksandr Zelentsov <aazelentsov@gid.ru>
2025-12-01 16:29:15 +0300

    Добавление элементов метамодели

--- packages/arch-repo/src/model/entities/links/assessment-link.entity.ts
+++ packages/arch-repo/src/model/entities/links/assessment-link.entity.ts
@@ -1,4 +1,3 @@
-import { BaseObject } from '@/model/abstract/base-object.abstract';
 import { Assessment } from '@/model/entities/archimate/motivation/assessment.entity';
 import { Entity, ManyToOne } from '@mikro-orm/core';
 import { IdentifiedObject } from '@/model/abstract/identified-object.abstract';
