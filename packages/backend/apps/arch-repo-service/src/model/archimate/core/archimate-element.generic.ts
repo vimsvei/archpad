260f55874ab582c12f06bcfa002022b63cff5c1f
Aleksandr Zelentsov <aazelentsov@gid.ru>
2026-01-04 22:36:31 +0300

    Дополнение модели репозитория

--- packages/backend/apps/arch-repo-service/src/model/archimate/core/archimate-element.generic.ts
+++ packages/backend/apps/arch-repo-service/src/model/archimate/core/archimate-element.generic.ts
@@ -4,4 +4,11 @@ import { NamedObject } from '@archpad/models';
 import { Employee } from '../../organisation/employee.entity';
 
 @Entity({ abstract: true })
-export abstract class ArchimateElementGeneric extends NamedObject {}
+export abstract class ArchimateElementGeneric extends NamedObject {
+  @ApiProperty({
+    description: 'tenant object',
+    format: 'uuid',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  tenantId!: string;
+}
---
+++ packages/backend/apps/arch-repo-service/src/model/archimate/core/service.generic.ts
@@ -0,0 +1,18 @@
+import { ArchimateElementGeneric } from '@/model/archimate/core/archimate-element.generic';
+import { Entity, Enum } from '@mikro-orm/core';
+import { LayerKind } from '@/model/enums/layer-kind.enum';
+import { ArchimateCode, HasuraTable } from '@archpad/models';
+
+@HasuraTable()
+@Entity({
+  tableName: 'services',
+  abstract: true,
+  discriminatorColumn: 'layer',
+})
+export abstract class ServiceGeneric extends ArchimateElementGeneric {
+  @Enum({ items: () => LayerKind, nativeEnumName: 'layer_kind_enum' })
+  layer!: LayerKind;
+
+  @ArchimateCode('SRV')
+  override code: string = undefined as any;
+}
