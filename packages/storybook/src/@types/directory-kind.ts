e090d73b847a1804161c9b73716b19759d8d10a0
Aleksandr Zelentsov <aazelentsov@gid.ru>
2026-02-13 02:04:22 +0300

    chore(storybook): remove legacy storybook package

--- packages/storybook/src/@types/directory-kind.ts
+++
@@ -1,9 +0,0 @@
-export enum DirectoryKind {
-  ORGANIZATION = 'Organization',
-  LOCATION = 'Location',
-  BUSINESS_ACTOR = 'Business Actor',
-  BUSINESS_ROLE = 'Business Role',
-  APPLICATION_COMPONENT = 'Application Component',
-  TECHNOLOGY_NODE = 'Technology Node',
-  STAKEHOLDER = 'Stakeholder',
-}
